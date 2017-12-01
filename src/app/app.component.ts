09dee0f84fdced05d04889a04ca6d23a0a4ef814
gjjagadeesh <jgunapati@corelogic.com>
2017-12-01 15:29:33 -0600

    Introducing Product Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
+  template: `<div><h1>{{pageTitle}}</h1><pm-products></pm-products></div>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+    templateUrl : './product-list.component.html',
+    selector : 'pm-products'
+})
+
+export class ProductListComponent
+{
+    pageTitle : string = 'Product List';
+}
