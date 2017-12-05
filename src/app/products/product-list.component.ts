8b3c4b26075fce1c3d868a33f4d1a6b857b0a971
gjjagadeesh <jgunapati@corelogic.com>
2017-12-05 15:50:01 -0600

    Introduced Property and Event Binding

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -8,6 +8,16 @@ import {Component} from '@angular/core';
 export class ProductListComponent
 {
     pageTitle : string = 'Product List';
+    imageWidth : number = 50;
+    imageMargin : number = 2;
+
+    showImage : boolean = false;
+    toogleImage() : void {
+        this.showImage = !this.showImage;
+    }
+
+    listFilter : string = 'cart';
+
     products : any[] = [
         {
             "productId": 1,
