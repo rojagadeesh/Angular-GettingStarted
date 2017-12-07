a7008c24421f76c262555bff75d9655a1b64518e
gjjagadeesh <jgunapati@corelogic.com>
2017-12-07 16:25:50 -0600

    09_RoutingWithParameters

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute,Router} from '@angular/router';
+import {IProduct} from './product';
+import {ProductService} from './product.service';
 
 @Component({
   selector: 'pm-product-detail.component',
@@ -8,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 export class ProductDetailComponent implements OnInit {
 
   pageTitle : string = 'Product Detail :';
-  constructor() { }
+  product : IProduct;
+  errorMessage : string;
+
+  constructor(private _route:ActivatedRoute,
+    private _router:Router,
+  private _productService: ProductService) 
+  { }
 
   ngOnInit() {
+    const param = this._route.snapshot.paramMap.get('id');
+    if(param)
+    {
+      const id = +param;
+      this.getProduct(id);
+    }
+    
+  }
+
+  getProduct(id:number)
+  {
+    this._productService.getProduct(id).subscribe(
+      product => this.product = product,
+      error => this.errorMessage = <any>error
+    );
+  }
+
+  onBack() : void{
+    this._router.navigate(['/products']);
   }
 
 }
--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -23,6 +23,12 @@ export class ProductService
             .catch(this.handleError);
     }
 
+    getProduct(id:number) : Observable<IProduct>
+    {
+        return this.getProducts()
+        .map((products:IProduct[]) => products.find(p => p.productId ===id));
+    }
+
     private handleError(err: HttpErrorResponse)
     {
         console.log(err.message);
